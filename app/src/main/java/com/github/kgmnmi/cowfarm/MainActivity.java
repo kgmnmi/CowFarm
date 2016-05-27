76e7fcd65e78b78108f7ebca88e41474a11040e2
Koga Minami <kgmnmi@gmail.com>
2016-05-27 16:08:16 +0900

    first commit

--- app/src/main/java/com/github/kgmnmi/cowfarm/MainActivity.java
+++ app/src/main/java/com/github/kgmnmi/cowfarm/MainActivity.java
@@ -1,15 +1,21 @@
 package com.github.kgmnmi.cowfarm;
 
 import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.media.MediaPlayer;
+import android.media.AudioManager;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -35,9 +41,20 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("", "onCreate");
         setContentView(R.layout.activity_main);
-        ImageView CowImage = (ImageView) this.findViewById(R.id.cow);
-        CowImage.setOnClickListener(this);
+        ImageView buttonCow = (ImageView) this.findViewById(R.id.button_cow);
+        buttonCow.setOnClickListener(this);
+        ImageButton buttonFeed = (ImageButton) this.findViewById(R.id.button_feed);
+        buttonFeed.setOnClickListener(this);
+        ImageButton buttonToilet = (ImageButton) this.findViewById(R.id.button_toilet);
+        buttonToilet.setOnClickListener(this);
+        ImageButton buttonShopping = (ImageButton) this.findViewById(R.id.button_shopping);
+        buttonShopping.setOnClickListener(this);
+        ImageView imageEaten = (ImageView) this.findViewById(R.id.eaten_feed);
+        imageEaten.setVisibility(View.INVISIBLE); //非表示処理
+        ImageView flushedToilet = (ImageView) this.findViewById(R.id.flushed_toilet);
+        flushedToilet.setVisibility(View.INVISIBLE); //非表示処理
     }
 
     @Override
@@ -72,16 +89,70 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        Log.d("", "onClick");
-        ImageView img = (ImageView) findViewById(R.id.cow);
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.motion);
-        animation.setFillAfter(false);   //終了後を保持
-        img.startAnimation(animation);
+        switch (v.getId()) {
+            case R.id.button_cow:
+                Log.d("", "onClick1");
+                ImageView img = (ImageView) findViewById(R.id.button_cow);
+                Animation animation = AnimationUtils.loadAnimation(this, R.anim.motion);
+                animation.setFillAfter(false);   //終了後を保持
+                img.startAnimation(animation);
+                MediaPlayer se = MediaPlayer.create(this, R.raw.cowsynthetic011);
+                se.start();
+                break;
+
+            case R.id.button_feed:
+                Log.d("", "onClick2");
+                final View unko = findViewById(R.id.flushed_toilet);
+                View eaten = findViewById(R.id.eaten_feed);
+                eaten.setVisibility(View.VISIBLE);
+                ScaleAnimation scale = new ScaleAnimation(1, 0f, 1, 0f); //imgを1倍から0倍に縮小
+                scale.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        unko.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+                scale.setDuration(3000); //3000msかけてアニメーションする
+                eaten.startAnimation(scale); //アニメーション適用
+                eaten.setVisibility(View.INVISIBLE);
+                break;
+
+            case R.id.button_toilet:
+                Log.d("", "onClick3");
+                View clean = findViewById(R.id.flushed_toilet);
+                AlphaAnimation alpha = new AlphaAnimation(1, 0.1f); //透明度を1から0.1に変化させる
+                alpha.setDuration(4000); //3000msかけてアニメーションする
+                clean.startAnimation(alpha); //アニメーション適用
+                clean.setVisibility(View.INVISIBLE);
+                break;
+
+            case R.id.button_shopping:
+                Log.d("", "onClick4");
+                break;
+
+            case R.id.eaten_feed:
+                Log.d("", "onClick4");
+                break;
+
+            case R.id.flushed_toilet:
+                Log.d("", "onClick5");
+                break;
+        }
     }
+
     @Override
     protected void onPause() {
         super.onPause();
         bgm.stop();
     }
-
 }
